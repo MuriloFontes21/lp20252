22e95bec6e2d09767182472211cf21aeb11c22ff
MuriloFontes21 <124307575+MuriloFontes21@users.noreply.github.com>
2025-08-17 18:02:22 +0000

    4 operações basicas

---
+++ exercicio11.py
@@ -0,0 +1,13 @@
+n1 = float(input("Digite o primeiro número: "))
+
+n2 = float(input("Digite o segundo número: "))
+
+adicao = n1 + n2
+sub = n1 - n2 
+mult = n1 * n2
+div = n1 / n2
+
+print("A adição é : ", adicao)
+print("A subtração é: ", sub)
+print("A multiplicação é: ", mult)
+print("A divisão é: ",div)
