d4a1f0d34df10f469dc6eeeadb59e9360652efd8
MuriloFontes21 <124307575+MuriloFontes21@users.noreply.github.com>
2025-08-17 05:48:30 +0000

    nome e endereço cliente

---
+++ exercicio7.py
@@ -0,0 +1,7 @@
+nome = input("Digite o nome do cliente: ")
+endereco = input("Digite o endereço do cliente: ")
+telefone = input("Digite o telefone do cliente: ")
+
+print("Nome:", nome)
+print("Endereço:", endereco)
+print("Telefone:", telefone)
