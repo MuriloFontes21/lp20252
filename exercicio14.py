82bdeb06487ab42ff6defde26ac5258829d28c27
MuriloFontes21 <124307575+MuriloFontes21@users.noreply.github.com>
2025-08-17 18:23:14 +0000

    parimetro e area do retangulo

---
+++ exercicio14.py
@@ -0,0 +1,8 @@
+base = float(input("Digite a base do retângulo: "))
+altura = float(input("Digite a altura do retângulo: "))
+
+perimetro = 2 * base + 2 * altura
+area = base * altura
+
+print("Perímetro do retângulo:", perimetro)
+print("Área do retângulo:", area)
