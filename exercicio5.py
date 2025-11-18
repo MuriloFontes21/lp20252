644fef0848e509ff621d688ddff78c2d6b16d75c
MuriloFontes21 <124307575+MuriloFontes21@users.noreply.github.com>
2025-11-18 08:40:49 -0400

    Delete exercicio5.py

--- exercicio5.py
+++
@@ -1,6 +0,0 @@
-n1 = int(input("Digite o primeiro numero inteiro: "))
-
-n2 = int(input("Digite o segundo numero inteiro: "))
-
-print("O primeiro número é: ", n1)
-print("O segundo número é: ", n2)
