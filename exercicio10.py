715744eebe842411e23605ada6bde00cc9d2292f
MuriloFontes21 <124307575+MuriloFontes21@users.noreply.github.com>
2025-11-18 08:39:07 -0400

    Delete exercicio10.py

--- exercicio10.py
+++
@@ -1,9 +0,0 @@
-n1 = float(input("Digite o primeiro número: "))
-
-n2 = float(input("Digite o segundo número: "))
-
-n3 = float(input("Digite o terceiro número: "))
-
-calc =(n1 + n2 + n3) / 3
-
-print("A média aritmética dos números digitados é: ", calc)
