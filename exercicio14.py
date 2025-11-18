a8cd6dd0f66c5e698ec42b179e9eb9de640e5d25
MuriloFontes21 <124307575+MuriloFontes21@users.noreply.github.com>
2025-11-18 08:39:48 -0400

    Delete exercicio14.py

--- exercicio14.py
+++
@@ -1,8 +0,0 @@
-base = float(input("Digite a base do retângulo: "))
-altura = float(input("Digite a altura do retângulo: "))
-
-perimetro = 2 * base + 2 * altura
-area = base * altura
-
-print("Perímetro do retângulo:", perimetro)
-print("Área do retângulo:", area)
