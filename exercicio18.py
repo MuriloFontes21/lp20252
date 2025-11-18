3b99f6c872a92d8026e94acbf85477713dbb506c
MuriloFontes21 <124307575+MuriloFontes21@users.noreply.github.com>
2025-11-18 08:39:56 -0400

    Delete exercicio18.py

--- exercicio18.py
+++
@@ -1,8 +0,0 @@
-velocidade = int(input("Velocidade média em km/h: "))
-tempo = float(input("Tempo de viagem em horas: "))
-
-distancia = tempo * velocidade
-consumocombustivel = distancia / 12
-
-print("A distância percorrida é: ", distancia)
-print("O consumo de combustivel foi de: ", consumocombustivel)
