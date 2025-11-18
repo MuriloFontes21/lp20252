9a6af616cf77fc908d6522dd68a7000a15fba67c
MuriloFontes21 <124307575+MuriloFontes21@users.noreply.github.com>
2025-11-18 08:41:07 -0400

    Delete exercicio7.py

--- exercicio7.py
+++
@@ -1,7 +0,0 @@
-nome = input("Digite o nome do cliente: ")
-endereco = input("Digite o endereço do cliente: ")
-telefone = input("Digite o telefone do cliente: ")
-
-print("Nome:", nome)
-print("Endereço:", endereco)
-print("Telefone:", telefone)
